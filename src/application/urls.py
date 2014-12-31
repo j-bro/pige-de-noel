5f8b98bb51139060cd6824d0dd7335aab844d270
Jeremy Brown <jj.brown@hotmail.com>
2014-12-30 23:34:26 -0500

    Added main pages to views and urls

--- src/application/urls.py
+++ src/application/urls.py
@@ -18,23 +18,12 @@ app.add_url_rule('/_ah/warmup', 'warmup', view_func=views.warmup)
 # Home page
 app.add_url_rule('/', 'home', view_func=views.home)
 
-# Say hello
-app.add_url_rule('/hello/<username>', 'say_hello', view_func=views.say_hello)
+app.add_url_rule('/manage_group', 'group_management', view_func=views.group_management)
 
-# Examples list page
-app.add_url_rule('/examples', 'list_examples', view_func=views.list_examples, methods=['GET', 'POST'])
-
-# Examples list page (cached)
-app.add_url_rule('/examples/cached', 'cached_examples', view_func=views.cached_examples, methods=['GET'])
+app.add_url_rule('/participant_view', 'participant_view', view_func=views.participant_view)
 
 # Contrived admin-only view example
-app.add_url_rule('/admin_only', 'admin_only', view_func=views.admin_only)
-
-# Edit an example
-app.add_url_rule('/examples/<int:example_id>/edit', 'edit_example', view_func=views.edit_example, methods=['GET', 'POST'])
-
-# Delete an example
-app.add_url_rule('/examples/<int:example_id>/delete', view_func=views.delete_example, methods=['POST'])
+# app.add_url_rule('/admin_only', 'admin_only', view_func=views.admin_only)
 
 
 ## Error handlers

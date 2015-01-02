3b554ef7b3e08259a7f6789a0e5de7d9a4e43a90
Jeremy Brown <jj.brown@hotmail.com>
2015-01-02 14:45:01 -0500

    Added gift list to models

--- src/application/models.py
+++ src/application/models.py
@@ -16,6 +16,13 @@ class FromTo(ndb.Model):
     from_user = ndb.StringProperty() # id of "FROM" participant
     to_user = ndb.StringProperty() # id of "TO" participant
     
+class GiftList(ndb.Model):
+    """
+    Gift lists for each participant in the exchange
+    """
+    user_id = ndb.StringProperty(required=True)
+    list = ndb.TextProperty(required=True, default='')
+    
 class Exchange(ndb.Model):
     """
     An exchange in which a group participates
@@ -26,17 +33,18 @@ class Exchange(ndb.Model):
     active = ndb.BooleanProperty(required=True, default=False)
     price_cap = ndb.IntegerProperty(required=True)
     draw = ndb.StructuredProperty(FromTo, repreated=True)
+    lists = ndb.StructuredProperty(List, repeated=True)
 
 class Group(ndb.Model):
     """Group of users"""
     name = ndb.StringProperty(required=True)
-    administrator = ndb.UserProperty(required=True)
+    administrator = ndb.StringProperty(required=True) # id of group administrator
     participants = ndb.StringProperty(repeated=True) # id of participants
     exchanges = ndb.StructuredProperty(Exchange, repeated=True)
     
 class User(ndb.Model):
     """App user"""
-    id = ndb.StringProperty(required=True) # user_id from Google login User object
+    user_id = ndb.StringProperty(required=True) # user_id from Google login User object
     name = ndb.StringProperty(required=True)
     first_login = ndb.DateTimeProperty(auto_now_add=True)
     last_login = ndb.DateTimeProperty(auto_now=True)
